bb2f2ce76988cc74845d21fbd46770f84a7680a5
Jonathan Sick <jonathansick@mac.com>
2013-07-26 15:26:57 -0400

    Refactor LibraryBuilder parameters
    All parameters for lib.dat are now set in `LibraryBuilder.__init__`.
    This allows us to create a valid `LibraryBuilder` instance without
    having to run `mklib` itseful. This is useful because `LibraryBuilder`
    is used by other classes as storage for paths and parameters.
    Paths to the isofile and lib.dat are also set at `__init__`.

--- starfisher/isolibrary.py
+++ starfisher/isolibrary.py
@@ -28,15 +28,44 @@ class LibraryBuilder(object):
     isoc_src_dir : str
         Name of the directory with raw isochrones, relative to the root of
         the StarFISH directory.
+    lib_dir : float
+        Directory where the isochrones will be installed by ``mklib``.
+    faint : float
+        Faint magnitude limit for output isochrone library (according to
+        filter at `mag0` index). Should be several mag fainter than the
+        data's faint limit.
+    dmag : float
+        Photometric distance between adjacent interpolated points.
+    dmod : float
+        Distance modulus (magnitudes).
+    gamma : float
+        Logarithmic IMF sloap (Salpeter = -1.35).
+    nmag : int
+        Number of bandpasses.
+    mag0 : int
+        Index (1-based) of reference magnitude for `faint` and `msto`.
+    iverb : int
+        Verbosity of `mklib`.
+        - 0 = silent
+        - 1 = screen messages
+        - 2 = extra output files
     """
-    def __init__(self, input_dir, isoc_src_dir):
+    def __init__(self, input_dir, isoc_src_dir, lib_dir,
+            faint=30., dmag=0.005, dmod=0.,
+            gamma=-1.35, nmag=2, mag0=1, iverb=0):
         super(LibraryBuilder, self).__init__()
         self.input_dir = input_dir
         self.isoc_src_dir = isoc_src_dir
-        self._isofile_path = None
-        self._iso_dir = None
-        self._libdat_pat = None
-        for dirname in (self.input_dir, self.isoc_src_dir):
+        self._iso_dir = lib_dir
+        self._isofile_path = os.path.join(self.input_dir, "isofile")
+        self._libdat_path = os.path.join(self.input_dir, "lib.dat")
+        self.faint = faint
+        self.dmag = dmag
+        self.dmod = dmod
+        self.gamma = gamma
+        self.nmag = nmag
+        self.mag0 = mag0
+        for dirname in (self.input_dir, self.isoc_src_dir, self._iso_dir):
             if not os.path.exists(dirname):
                 os.makedirs(dirname)
 
@@ -78,7 +107,6 @@ class LibraryBuilder(object):
             output_path = os.path.join(self._iso_dir, basepath)
             t.add_row((float(age_str), p, output_path, 100.))
         
-        self._isofile_path = os.path.join(self.isoc_src_dir, "isofile")
         self._write_isofile(t)
 
     def _write_isofile(self, tbl):
@@ -86,43 +114,12 @@ class LibraryBuilder(object):
         if os.path.exists(self._isofile_path): os.remove(self._isofile_path)
         tbl.write(self._isofile_path, format='ascii.no_header', delimiter=' ')
 
-    def _build_libdat(self, faint=30., dmag=0.005, dmod=0.,
-            gamma=-1.35, nmag=2, mag0=1, iverb=0):
+    def _build_libdat(self):
         """Build the library data file, used by `mklib`.
-
-        Parameters
-        ----------
-
-        faint : float
-            Faint magnitude limit for output isochrone library (according to
-            filter at `mag0` index). Should be several mag fainter than the
-            data's faint limit.
-        dmag : float
-            Photometric distance between adjacent interpolated points.
-        dmod : float
-            Distance modulus (magnitudes).
-        gamma : float
-            Logarithmic IMF sloap (Salpeter = -1.35).
-        nmag : int
-            Number of bandpasses.
-        mag0 : int
-            Index (1-based) of reference magnitude for `faint` and `msto`.
-        iverb : int
-            Verbosity of `mklib`.
-            - 0 = silent
-            - 1 = screen messages
-            - 2 = extra output files
         """
-        self.faint = faint
-        self.dmag = dmag
-        self.dmod = dmod
-        self.gamma = gamma
-        self.nmag = nmag
-        self.mag0 = mag0
         datstr = "%s\n%.3f\n%.3f\n%.3f\n%.3f\n%i\n%i\n%i" % \
-                (self._isofile_path, faint, dmag, dmod, gamma,
-                nmag, mag0, iverb)
-        self._libdat_path = os.path.join(self.input_dir, "lib.dat")
+                (self._isofile_path, self.faint, self.dmag, self.dmod,
+                self.gamma, self.nmag, self.mag0, self.iverb)
         if os.path.exists(self._libdat_path): os.remove(self._libdat_path)
         with open(self._libdat_path, 'w') as f:
             f.write(datstr)
@@ -135,40 +132,12 @@ class LibraryBuilder(object):
         for p in paths:
             os.remove(p)
 
-    def install(self, lib_dir='iso', **kwargs):
+    def install(self):
         """Runs `mklib` to install the parsed isochrones into the isochrone
         library directory.
-
-        Parameters
-        ----------
-
-        lib_dir : float
-            Directory where the isochrones will be installed by ``mklib``.
-        faint : float
-            Faint magnitude limit for output isochrone library (according to
-            filter at `mag0` index). Should be several mag fainter than the
-            data's faint limit.
-        dmag : float
-            Photometric distance between adjacent interpolated points.
-        dmod : float
-            Distance modulus (magnitudes).
-        gamma : float
-            Logarithmic IMF sloap (Salpeter = -1.35).
-        nmag : int
-            Number of bandpasses.
-        mag0 : int
-            Index (1-based) of reference magnitude for `faint` and `msto`.
-        iverb : int
-            Verbosity of `mklib`.
-            - 0 = silent
-            - 1 = screen messages
-            - 2 = extra output files
         """
-        self._iso_dir = lib_dir
-        if not os.path.exists(self._iso_dir):
-            os.makedirs(self._iso_dir)
         self._build_isofile()
-        self._build_libdat(**kwargs)
+        self._build_libdat()
         self._clean_isodir()
         subprocess.call('./mklib < %s' % self._libdat_path, shell=True)
         self._check_library()
