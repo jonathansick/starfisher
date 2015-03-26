99826cc14496cccf4fc8d709fef8188b03014ade
Jonathan Sick <jonathansick@mac.com>
2015-03-26 13:58:54 -0400

    Easy import for synth classes

--- starfisher/__init__.py
+++ starfisher/__init__.py
@@ -1,1 +1,4 @@
 from starfisher.isolibrary import LibraryBuilder  # NOQA
+from starfisher.synth import Synth  # NOQA
+from starfisher.synth import Lockfile  # NOQA
+from starfisher.synth import ExtinctionDistribution  # NOQA
