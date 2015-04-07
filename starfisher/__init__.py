e238c8f5f7b752c12813bae2ec5f3e528c3a97a7
Jonathan Sick <jonathansick@mac.com>
2015-04-07 11:34:00 -0400

    Refactor ExtinctionDistribution to own module

--- starfisher/__init__.py
+++ starfisher/__init__.py
@@ -4,7 +4,7 @@ from starfisher.lockfile import Lockfile  # NOQA
 from starfisher.lockfile import Lockfile  # NOQA
 from starfisher.plane import ColorPlane  # NOQA
 from starfisher.plane import Mask  # NOQA
-from starfisher.synth import ExtinctionDistribution  # NOQA
+from starfisher.dust import ExtinctionDistribution  # NOQA
 from starfisher.crowd import MockNullCrowdingTable  # NOQA
 from starfisher.crowd import ExtantCrowdingTable  # NOQA
 from starfisher.sfh import SFH  # NOQA
--- starfisher/synth.py
+++ starfisher/synth.py
@@ -11,9 +11,6 @@ import subprocess
 import logging
 import multiprocessing
 
-import numpy as np
-from astropy.table import Table
-
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 import matplotlib.gridspec as gridspec
@@ -314,55 +311,3 @@ def _run_synth(synth_config_path):
         command = "./synth < {0}".format(synth_config_path)
         print(command)
         subprocess.call(command, shell=True)
-
-
-class ExtinctionDistribution(object):
-    """Create an extinction distribution file for :class:`Synth`.
-
-    Synthesized stars will have extinction values drawn randomly from samples
-    in the extintion distribution. Uniform extinction can be implemented by
-    using only one extinction value.
-    """
-    def __init__(self):
-        super(ExtinctionDistribution, self).__init__()
-        self._extinction_array = None
-
-    def set_samples(self, extinction_array):
-        """Set a 1D array of extinction values.
-
-        Parameters
-        ----------
-        extinction_array : ndarray, (n,)
-            A 1D array of extinction sample values (in magnitudes).
-        """
-        # TODO check shape of extinction array
-        self._extinction_array = extinction_array
-
-    def set_uniform(self, extinction):
-        """Set uniform extinction.
-
-        Parameters
-        ----------
-        extinction : float
-            The uniform extinction value (in magnitudes).
-            I.e., set `extinction=0.` for no
-            extinction.
-        """
-        self._extinction_array = np.array([extinction])
-
-    def write(self, path):
-        """Write the extinction file to `path`.
-
-        Parameters
-        ----------
-        path : str
-            Path where extinction file is written, relative to the StarFISH
-            directory.
-        """
-        full_path = os.path.join(starfish_dir, path)
-        dirname = os.path.dirname(full_path)
-        if not os.path.exists(dirname):
-            os.makedirs(dirname)
-
-        t = Table([self._extinction_array], names=['A'])
-        t.write(full_path, format='ascii.no_header', delimiter=' ')
